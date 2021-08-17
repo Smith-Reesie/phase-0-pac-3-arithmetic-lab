c682875c51e37cf84d94d1a8be32dad39b8f67bc
Maurice Smith <Xmaurice.smith@gmail.com>
2021-08-17 15:02:32 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+function subtract(num1, num2) {
+    return num1 - num2
+}
+function multiply(num1, num2){
+    return num1 * num2
+}
+function divide(num1, num2){
+    return num1 / num2
+}
+function increment(n){
+    return n += 1
+}
+
+function decrement(n){
+    return n -= 1
+}
+function makeInt(n){
+parseInt(n, 10) 
+return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    parseFloat(n, 10)
+    return parseFloat(n, 10)
+}
